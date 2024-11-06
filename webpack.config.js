a26d8fe3741d6ff676496b4c6eff492b2d7c0810
Vladimir <veb455@mail.ru>
2024-11-06 13:57:31 +0300

    header-navigation

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,38 @@
-var path = require("path")
+var path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, "./src/ts/bin/index.ts"),
-    output: {
-        path: path.resolve(__dirname, "public"),
-        filename: "main.js"
-    },
-    mode: "production",
-    plugins: [new MiniCssExtractPlugin()],
-    module: {
-        rules: [
-            {
-                test: /\.s?css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(), new TerserPlugin()
-        ]
-    }
-}
+  entry: path.resolve(__dirname, "./src/ts/bin/index.ts"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+  mode: "production",
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+};
