51822eb2ff884b553f97574f0f349aefcae05a08
Vladimir <veb455@mail.ru>
2024-11-06 09:35:41 +0300

    wepback-r

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 var path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, "./src/js/bin/index.ts"),
-    output: path.resolve(__dirname, "public"),
+    entry: path.resolve(__dirname, "./src/ts/bin/index.ts"),
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "main.js"
+    },
     mode: "production",
+    plugins: [new MiniCssExtractPlugin()],
     module: {
-        rule: []
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(), new TerserPlugin()
+        ]
     }
 }
