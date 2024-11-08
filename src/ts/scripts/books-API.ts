37c35bb55d493e456d48f43dbf514bb86f3f7ae0
Vladimir <veb455@mail.ru>
2024-11-08 16:42:42 +0300

    tired

--- src/ts/scripts/books-API.ts
+++ src/ts/scripts/books-API.ts
@@ -24,8 +24,21 @@ const categories = document.querySelectorAll(".category-list_li") as NodeListOf<
 
 let currentCategory:string = "Architecture"
 
-const createHtmlBook = (obj: BookVolume):string => {
-    return ``
+const createHtmlBook = (authors:string[], description:string, price:number, img?:string, ratingsCount?:number, averageRating?:number):string => {
+    return `<div class="book">
+    <img src="" alt="" class="book-img">
+    <div class="book-info">
+        <p class="book-info_author"></p>
+        <h1 class="book-info_h1"></h1>
+        <div class="book-rating">
+            <div class="book-rating-stars"><img src="" alt="" class="book-rating-stars_star"></div>
+            <div class="book-rating-counting"><p class="book-rating-counting_count"></p></div>
+        </div>
+        <p class="book-info_description"></p>
+        <div class="book-info_price"></div>
+        <button class="book-info_button">IN THE CART</button>
+    </div>
+</div>`
 }
 
 const getBookList = (token:string):void => {
